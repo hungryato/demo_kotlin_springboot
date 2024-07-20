a50d59b6d0590a43d2e05f93f5b4b92dcd84f264
hungryato <hungryato17@icloud.com>
2024-07-20 22:00:13 +0900

    Retrieve messages by id

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,14 +2,15 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.annotation.Id
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
+import org.springframework.jdbc.core.query
 
 @SpringBootApplication
 class DemoApplication
@@ -22,6 +23,16 @@ fun main(args: Array<String>) {
 class MessageController(val service: MessageService) {
 	@GetMapping("/")
 	fun index(): List<Message> = service.findMessages()
+
+	/**
+	 * Retrieving a value from the context path
+	 *
+	 * The message id is retrieved from the context path by the Spring Framework as you annotated the new function by @GetMapping("/{id}").
+	 * By annotating the function argument with @PathVariable, you tell the framework to use the retrieved value as a function argument.
+	 * The new function makes a call to MessageService to retrieve the individual message by its id.
+	 */
+	@GetMapping("/{id}")
+	fun index(@PathVariable id: String): List<Message> = service.findMessageById(id)
 
 	@PostMapping("/")
 	fun post(@RequestBody message: Message) {
@@ -37,14 +48,21 @@ class MessageService(val db: JdbcTemplate) {
 		Message(response.getString("id"), response.getString("text"))
 	}
 
+	/**
+	 * vararg argument position in the parameter list
+	 *
+	 * The query() function takes three arguments:
+	 * - SQL query string that requires a parameter to run
+	 * - `id`, which is a parameter of type String
+	 * - RowMapper instance is implemented by a lambda expression
+	 * The second parameter for the query() function is declared as a variable argument (vararg).
+	 * In Kotlin, the position of the variable arguments parameter is not required to be the last in the parameters list.
+	 */
+	fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id) { response, _ ->
+		Message(response.getString("id"), response.getString("text"))
+	}
+
 	fun save(message: Message) {
-		/**
-		 * Elvis operator - ?:
-		 *
-		 * The code message.id ?: UUID.randomUUID().toString() uses the Elvis operator (if-not-null-else shorthand) ?:.
-		 * If the expression to the left of ?: is not null, the Elvis operator returns it; otherwise, it returns the expression to the right.
-		 * Note that the expression on the right-hand side is evaluated only if the left-hand side is null.
-		 */
 		val id = message.id ?: UUID.randomUUID().toString()
 		db.update(
 			"insert into messages values (?, ?)",
